01edf2ccad9a94cd0a826ba6e7f4cef403c0074c
Maitri Chattopadhyay <maitrichattopadhyay@atlashal-12.local>
2017-11-24 23:51:57 -0700

    Enabled Firebase Push Notification (Only console)

---
+++ Mobile_App/app/src/main/java/com/example/maitrichattopadhyay/lowpower/MyFirebaseMessagingService.java
@@ -0,0 +1,29 @@
+package com.example.maitrichattopadhyay.lowpower;
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+/**
+ * Created by maitrichattopadhyay on 11/24/17.
+ */
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = "FCM Service";
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage)
+    {
+        // TODO: Handle FCM messages here.
+        // If the application is in the foreground handle both data and notification messages here.
+        // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated.
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+        Log.d(TAG, "Notification Message Body: " + remoteMessage.getData().get("message"));
+    }
+
+
+}
